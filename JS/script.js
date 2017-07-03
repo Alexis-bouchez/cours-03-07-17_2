9f731a8159d99f9af68d910335085a4e6540c159
popschool <popschool@debian>
2017-07-03 12:00:21 +0200

    -

---
+++ JS/script.js
@@ -0,0 +1,8 @@
+var print = function() {
+  var box = document.getElementById("box");
+  box.innerHTML =
+    "Nom : " + document.getElementById("lastname").value + "<br>" +
+    "Prénom : " + document.getElementById("firstname").value + "<br>" +
+    "Age : " + document.getElementById("age").value + "<br>" +
+    "Adresse : " + document.getElementById("add").value + "<br>";
+}
